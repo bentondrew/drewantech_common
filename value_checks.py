4545bce2f5df31198681573321d2ee3a5772d50c
Benton Drew <benton.s.drew@drewantech.com>
2016-04-28 13:08:14 -0700

    Implement valid_directory function in value_checks.py

--- value_checks.py
+++ value_checks.py
@@ -8,3 +8,14 @@ def is_number_type_not_complex(value_to_check):
     if type(value_to_check) is not float:
       return False
   return True
+
+
+def valid_directory(directory):
+  import os
+  if type(directory) is not str:
+    raise ValueError('The provided directory, {}, is not a str type.'
+                     .format(directory))
+  if not os.path.isdir(directory):
+    raise OSError('The provided directory, {}, is not a valid directory '
+                  'location.'.format(directory))
+  return directory
